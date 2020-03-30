493d400eb88b403e2c5a46c5f0a791b3f92e4e2b
jokerguo <1977036313@qq.com>
2020-03-30 11:49:22 +0800

    切换loading状态

--- src/icon.vue
+++ src/icon.vue
@@ -6,7 +6,7 @@
 
 <script>
   export default {
-    props:['name']
+    props:['name'],
   }
 </script>
 
@@ -14,8 +14,5 @@
   .g-icon{
     width: 1em;
     height: 1em;
-    vertical-align: -0.15em;
-    fill: currentColor;
-    overflow: hidden;
   }
 </style>
