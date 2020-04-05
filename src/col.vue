6f45fdec084e3e98adb354761c090d9ddf5e5f0c
jokerguo <1977036313@qq.com>
2020-04-05 17:00:08 +0800

    重构row col组件

--- src/col.vue
+++ src/col.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
-       :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}"
-  >
-    <div style="border: 1px solid blue;height: 100px;">
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClass" :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 
@@ -17,13 +13,30 @@
       },
       offset: {
         type: [Number, String]
-      }
+      },
     },
     data() {
       return {
         gutter: 0
       }
+    },
+    computed: {
+      colClass() {
+        let {span, offset} = this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle() {
+        return {
+          paddingLeft: this.gutter / 2 + 'px',
+          paddingRight: this.gutter / 2 + 'px'
+        }
+      }
     }
+
+
   }
 </script>
 
