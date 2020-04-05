ca5f7907b2d7d4f526ec0bb9acd37d2b7f00dde2
jokerguo <1977036313@qq.com>
2020-04-05 16:43:51 +0800

    基本实现row col的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+       :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}"
+  >
+    <div style="border: 1px solid blue;height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -9,7 +13,15 @@
     name: 'GuluCol',
     props: {
       span: {
-        type : [Number,String]
+        type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      }
+    },
+    data() {
+      return {
+        gutter: 0
       }
     }
   }
@@ -18,14 +30,19 @@
 <style lang="scss" scoped>
   .col {
     width: 50%;
-    height: 100px;
-    background-color: grey;
-    border: 1px solid red;
+    padding: 0 10px;
 
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n /24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n /24) * 100%;
       }
     }
   }
