47c2351a4560cd84885f577c7b8f655c00b789ea
jokerguo <1977036313@qq.com>
2020-04-06 16:12:44 +0800

    添加toast plugin

--- src/app.js
+++ src/app.js
@@ -10,29 +10,34 @@ import Header from './header'
 import Content from './content'
 import Footer from './footer'
 import Sider from './sider'
+import Toast from './toast'
+import Plugin from './plugin'
 
-Vue.component('g-button',Button)
-Vue.component('g-button-group',ButtonGroup)
-Vue.component('g-icon',Icon)
-Vue.component('g-input',Input)
-Vue.component('g-col',Col)
-Vue.component('g-row',Row)
-Vue.component('g-layout',Layout)
-Vue.component('g-header',Header)
-Vue.component('g-content',Content)
-Vue.component('g-footer',Footer)
-Vue.component('g-sider',Sider)
+Vue.component('g-button', Button)
+Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-icon', Icon)
+Vue.component('g-input', Input)
+Vue.component('g-col', Col)
+Vue.component('g-row', Row)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-sider', Sider)
+Vue.component('g-toast', Toast)
+Vue.use(Plugin)
 new Vue({
-  el: '#app',
-  data : {
-    loading1: false,
-    loading2: true,
-    loading3: false,
-    message : 'hi'
-  },
-  methods:{
-    inputChange(e){
-      console.log(e)
-    }
+    el: '#app',
+    data: {
+      loading1: false,
+      loading2: true,
+      loading3: false,
+      message: 'hi'
+    },
+    methods: {
+      showToast(){
+        this.$toast('我是message')
+      }
+    },
   }
-})
+)
