520754cb97c316dbb73034f7dd52df31ead2973b
jokerguo <1977036313@qq.com>
2020-04-09 13:30:32 +0800

    添加tabs相关组件 props相关属性

---
+++ src/tabs.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tabs">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:{
+      selected:{
+        type: String,
+        require : true
+      },
+      direction:{
+        type: String,
+        default : 'Horizontal',
+        validator(value){
+          return ['Horizontal','vertical'].indexOf(value) >= 0
+        }
+      },
+    },
+    created() {
+      this.$emit('update:selected','xxx')
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
