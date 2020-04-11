51e6a8e43951d762c0b6b082e6059bbe308ef7da
jokerguo <1977036313@qq.com>
2020-04-11 20:00:57 +0800

    修改button组件 css声明的全局变量修改为 scss的变量

--- src/button.vue
+++ src/button.vue
@@ -35,6 +35,15 @@
 </script>
 
 <style lang="scss" scoped>
+
+  $button-height:32px;
+  $font-size: 14px;
+  $button-bg: white;
+  $button-active-bg: #eee;
+  $border-radius: 4px;
+  $color: #333;
+  $border-color: #999;
+  $border-color-hover: #666;
   @keyframes spin {
     0% {
       transform: rotate(0deg)
@@ -44,20 +53,20 @@
     }
   }
   .g-button {
-    height: var(--button-height);
-    font-size: var(--font-size);
+    height: $button-height;
+    font-size: $font-size;
     padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
+    border-radius: $border-radius;
+    border: 1px solid $border-color;
+    background: $button-bg;
     display: inline-flex;
     justify-content: center;
     align-items: center;
     vertical-align: middle;
 
-    &:hover {border-color: var(--border-color-hover);}
+    &:hover {border-color: $border-color-hover;}
 
-    &:active {background-color: var(--button-active-bg);}
+    &:active {background-color: $button-active-bg;}
 
     &:focus {outline: none;}
 
