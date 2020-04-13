2c8dd1d9f5c829c35ada2815466d0784513cf393
jokerguo <1977036313@qq.com>
2020-04-13 22:30:10 +0800

    上传npm

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 import Button from './src/button'
+import Icon from './src/icon'
 import ButtonGroup from './src/button-group'
-import Icon from './src/svg'
+import Input from './src/input'
+import Col from './src/col'
+import Row from './src/row'
+import Layout from './src/layout'
+import Header from './src/header'
+import Content from './src/content'
+import Footer from './src/footer'
+import Sider from './src/sider'
+import Toast from './src/toast'
+import Plugin from './src/plugin'
+import Tabs from './src/tabs'
+import TabsHead from './src/tabs-head'
+import TabsBody from './src/tabs-body'
+import TabsItem from './src/tabs-item'
+import TabsPane from './src/tabs-pane'
+import Popover from './src/popover'
+import Collapse from './src/collapse'
+import CollapseItem from './src/collapse-item'
 
 
-export {Button,ButtonGroup,Icon}
+export {Button,ButtonGroup,Icon,Input,Col,Row,Layout,Header,Content,Footer,Sider,Toast,
+Plugin,Tabs,TabsHead,TabsPane,TabsItem,TabsBody,Popover,Collapse,CollapseItem}
 
